6620368f2b2eccc9e198e0951cc30b9e11c6624a
David Aditya Winarto <David Aditya Winarto@LAPTOP-1HV2CE3U>
2019-09-22 00:35:03 +0700

    PBO2-10118071-Latihan9-Teks

---
+++ src/pbo2/pkg10118071/latihan9/teks/PBO210118071Latihan9Teks.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118071.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA              : David Aditya Winarto
+ * KELAS             : PBO2
+ * NIM               : 10118071
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ *                     tipe data karakter
+ * 
+ */
+public class PBO210118071Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
